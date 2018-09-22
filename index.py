63cd6e269b55b21d5977e6f05395e03040cbbe79
mattperkins <github@mattperkins.com>
2018-09-22 19:23:43 +0100

    add auth routes

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '316be1e776a2a03baf117defaf3e75e1'
 
 posts = [
  # dictionary 1
@@ -26,6 +30,19 @@ def home():
 @app.route("/about")
 def about():
  return render_template('about.html')
+
+@app.route("/signup")
+def signup():
+ form = RegistrationForm()
+ return render_template('signup.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+ form = LoginForm()
+ return render_template('login.html', title='Login', form=form)
+
+
+
 
  if __name__ == '__main__':
   app.run(debug=True)
