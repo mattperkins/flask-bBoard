8e62fb41bc3162ab8bf0dd57d7d23dbaef5c0a2d
mattperkins <github@mattperkins.com>
2018-09-22 08:24:18 +0100

    add dummy data (dictionaries)

--- index.py
+++ index.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+ # dictionary 1
+ {
+  'author': 'DORMshed',
+  'title': 'Insert One'
+  'Content': 'Lorem Ipsum',
+  'date_posted', 'September 22, 2018'
+ }
+ # dictionary 2
+ {
+  'author': 'DORMshed',
+  'title': 'Insert Two'
+  'Content': 'Lorem Ipsum',
+  'date_posted', 'September 21, 2018'
+ }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
- return render_template('home.html')
+ return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
